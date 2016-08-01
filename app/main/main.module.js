73ce6f07a574242bf1eafb95f77414ee5cee5f22
Marx JMoura <marx.jmoura@gmail.com>
2016-08-01 12:44:02 -0300

    Refactoring into main module

--- app/main/main.module.js
+++ app/main/main.module.js
@@ -3,6 +3,7 @@
   var module = angular.module('main', ['ngRoute', 'oc.lazyLoad']);
 
   module.config(location);
+  module.config(stringVersion);
   module.run(modules);
 
   location.$inject = ['$locationProvider'];
@@ -15,6 +16,12 @@
     });
   }
 
+  function stringVersion() {
+    String.prototype.newVersion = function() {
+      return this + '?v=' + new Date().getTime();
+    };
+  }
+
   function modules($ocLazyLoad) {
     $ocLazyLoad.load([
       'app/access-control/access-control.module.js'.newVersion(),
@@ -23,8 +30,3 @@
   }
 
 })();
-
-// TODO: Move to a better place
-String.prototype.newVersion = function() {
-  return this + '?v=' + new Date().getTime();
-};
