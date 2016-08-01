8898d87a29caf61194388946f7f4d73390107e77
Marx JMoura <marx.jmoura@gmail.com>
2016-08-01 15:54:52 -0300

    Refactoring into dashboard module

--- app/dashboard/dashboard.module.js
+++ app/dashboard/dashboard.module.js
@@ -14,14 +14,12 @@
         templateUrl: 'app/dashboard/views/home.html'.newVersion(),
         controller: 'homeCtrl',
         controllerAs: 'home',
-        resolve: {
-          lazy: ['$ocLazyLoad', function($ocLazyLoad) {
-            return $ocLazyLoad.load([
-              '/app/dashboard/controllers/home.controller.js'.newVersion(),
-              '/app/dashboard/services/welcome.service.js'.newVersion()
-            ]);
-          }]
-        }
+        resolve: ['$ocLazyLoad', function($ocLazyLoad) {
+          return $ocLazyLoad.load([
+            '/app/dashboard/controllers/home.controller.js'.newVersion(),
+            '/app/dashboard/services/welcome.service.js'.newVersion()
+          ]);
+        }]
       });
   }
 
