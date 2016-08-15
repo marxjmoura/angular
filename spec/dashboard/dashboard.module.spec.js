f26330ff0266f00b50ff86999ca9b133f80ea2b6
Marx JMoura <marx.jmoura@gmail.com>
2016-08-15 14:06:59 -0300

    Spec code refactoring

--- spec/dashboard/dashboard.module.spec.js
+++ spec/dashboard/dashboard.module.spec.js
@@ -1,11 +1,14 @@
 describe('dashboard module', function() {
+  var $route;
+
   beforeEach(module('dashboard'));
+  beforeEach(inject(function($injector) {
+    $route = $injector.get('$route');
+  }));
 
   it('should map home route', function() {
-    inject(function($route) {
-      expect($route.routes['/'].controller).toBe('HomeCtrl');
-      expect($route.routes['/'].controllerAs).toBe('home');
-      expect($route.routes['/'].templateUrl).toEqual('app/dashboard/views/home.html');
-    });
+    expect($route.routes['/'].controller).toBe('HomeCtrl');
+    expect($route.routes['/'].controllerAs).toBe('home');
+    expect($route.routes['/'].templateUrl).toEqual('app/dashboard/views/home.html');
   });
 });
